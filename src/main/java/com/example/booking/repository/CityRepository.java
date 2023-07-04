4f7b5a9de28e237eecfe257d7ec81a675e55f7e2
amirali1999 <85882122+amirali1999@users.noreply.github.com>
2023-07-04 13:19:02 +0330

    add find by label and value

--- src/main/java/com/example/booking/repository/CityRepository.java
+++ src/main/java/com/example/booking/repository/CityRepository.java
@@ -4,6 +4,10 @@ import com.example.booking.model.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CityRepository extends JpaRepository<City,Long> {
+    Optional<City> findByLabel(String label);
+    Optional<City> findByValue(String value);
 }
